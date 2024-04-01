79cfbc42b24b5d9d529a6d5eb5c799af4bc15df2
MaksymSemk <maksumsemen2006@gmail.com>
2024-04-01 16:26:19 +0300

    Added method to readFiles

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         storage = Storage.getInstance();
+        storage.readFiles();
         mainMenu = new MainMenu();
     }
 }
